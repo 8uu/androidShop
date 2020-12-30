55c5a4faadb98ef5a22d1047b777eefacb6fd00d
ponomaREG <ponomarcomru@gmail.com>
2020-12-30 23:29:06 +0300

    begin item_cart_product.xml

---
+++ app/src/main/java/com/ponomar/shoper/model/entities/Cart.kt
@@ -0,0 +1,22 @@
+package com.ponomar.shoper.model.entities
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
+import androidx.room.PrimaryKey
+
+
+@Entity(foreignKeys = [ForeignKey(
+    entity = Product::class,
+    parentColumns = arrayOf("id"),
+    childColumns = arrayOf("pid"),
+    onDelete = CASCADE,
+    onUpdate = CASCADE,
+    deferred = true
+)]
+)
+data class Cart(
+    @PrimaryKey(autoGenerate = true) val id:Int,
+    val pid:Int,
+    val quantity:Int
+    )
