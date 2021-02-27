9a8efbc38876d41318cb9b3c086e7dac907c470a
ponomaREG <ponomarcomru@gmail.com>
2021-02-27 19:49:33 +0300

    test user network

---
+++ app/src/test/java/com/ponomar/shoper/network/UserServiceTest.kt
@@ -0,0 +1,42 @@
+package com.ponomar.shoper.network
+
+import com.ponomar.shoper.model.body.EmailBody
+import com.ponomar.shoper.model.body.TokenBody
+import com.skydoves.sandwich.ApiResponse
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNull
+
+class UserServiceTest:ApiAbstract<UserService>() {
+
+    private lateinit var service:UserService
+
+    @Before
+    fun initService(){
+        service = createService(UserService::class.java)
+    }
+
+    @Test
+    fun updateUserEmailTest() = runBlocking{
+        enqueueResponse("/UpdateEmailResponse.json")
+        val response = service.updateUserInfo(EmailBody("email@test.com","TEST_TOKEN"))
+        val responseData = requireNotNull(response as ApiResponse.Success).data!!
+        mockWebServer.takeRequest()
+
+        assertEquals("status",0,responseData.status)
+    }
+
+    @Test
+    fun fetchUserInfoTest() = runBlocking{
+        enqueueResponse("/UserResponse.json")
+        val response = service.fetchUserInfo(TokenBody("TEST_TOKEN"))
+        val data = requireNotNull(response as ApiResponse.Success).data!!
+        mockWebServer.takeRequest()
+
+        assertEquals("userID",1,data.data!!.id)
+        assertEquals("status",0,data.status)
+        assertNull("emailNull",data.data!!.email)
+    }
+}
