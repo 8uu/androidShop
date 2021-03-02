9678f485002b9b30a9f565795d04162a9803b872
ponomaREG <ponomarcomru@gmail.com>
2021-03-02 21:03:08 +0300

    test repository user begin

---
+++ app/src/test/java/com/ponomar/shoper/repository/UserRepositoryTest.kt
@@ -0,0 +1,71 @@
+package com.ponomar.shoper.repository
+
+import app.cash.turbine.test
+import com.nhaarman.mockitokotlin2.atLeastOnce
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import com.ponomar.shoper.common.MockUtilUnit.mockUser
+import com.ponomar.shoper.model.UserResponse
+import com.ponomar.shoper.model.body.TokenBody
+import com.skydoves.sandwich.ApiResponse
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.Assert.assertEquals
+import retrofit2.Response
+import kotlin.time.ExperimentalTime
+
+class UserRepositoryTest: ApiAbstract() {
+    private lateinit var repository: UserRepository
+
+    @Before
+    fun initRepository(){
+        repository = UserRepository(client,daoHolder)
+    }
+
+    @ExperimentalCoroutinesApi
+    @ExperimentalTime
+    @Test
+    fun fetchUserInfoFromNetworkTest() = runBlocking{
+        val mockUserResponse = UserResponse(0,mockUser())
+
+        whenever(userDAO.getUser()).thenReturn(null)
+        whenever(userService.fetchUserInfo(TokenBody("TEST_TOKEN"))).thenReturn(ApiResponse.of { Response.success(mockUserResponse) })
+
+        repository.fetchUserInfo(
+                "TEST_TOKEN",
+                onComplete = {},
+                onError = {},
+                isForceUpdate = false
+        ).test{
+            val item = expectItem()
+            assertEquals(1,item.id)
+            assertEquals("User",item.firstName)
+            expectComplete()
+        }
+
+        verify(userDAO, atLeastOnce()).nukeTable()
+        verify(userDAO, atLeastOnce()).insert(mockUserResponse.data!!)
+    }
+
+    @ExperimentalCoroutinesApi
+    @ExperimentalTime
+    @Test
+    fun fetchUserInfoFromDbTest(): Unit = runBlocking{
+        val mockUser = mockUser()
+        whenever(userDAO.getUser()).thenReturn(mockUser)
+
+        repository.fetchUserInfo(
+                "TEST_TOKEN",
+                onComplete = {},
+                onError = {}
+        ).test {
+            val item = expectItem()
+            assertEquals(1,item.id)
+            assertEquals("User",item.firstName)
+            expectComplete()
+        }
+        verify(userDAO, atLeastOnce()).getUser()
+    }
+}
